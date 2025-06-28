e814fde8376e16a7eaa63b6296554e458acfb2e1
AngelPenaGarcia05 <angelpenaGar@hotmail.com>
2025-06-28 08:50:36 -0500

    chore: Cambiar la configuración para netlify

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import bootstrap from './main.server';
+import { render } from '@netlify/angular-runtime/common-engine'
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
@@ -63,5 +64,7 @@ if (isMainModule(import.meta.url)) {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
-
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  return await render(commonEngine)
+}
 export default app;
