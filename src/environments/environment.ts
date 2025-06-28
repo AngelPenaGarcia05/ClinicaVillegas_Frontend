f15006ba90267916b90a8be21addaaec2f455b28
AngelPenaGarcia05 <angelpenaGar@hotmail.com>
2025-06-28 13:33:33 -0500

    fix(url): Corrección de URL del backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'clinicavillegasbackend-production.up.railway.app/api'
+    apiUrl: 'https://clinicavillegasbackend-production.up.railway.app/api'
 };
